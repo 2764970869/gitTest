4b508a7661e4c3b66b4f6b518de796ee22317f82
gang <2764970869@qq.com>
2022-04-09 11:14:43 +0800

    push commit

--- main/java/test.java
+++ main/java/test.java
@@ -7,5 +7,6 @@ public class test {
         System.out.println("hello world4!!!!");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
